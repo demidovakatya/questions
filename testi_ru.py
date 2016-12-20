702da232b04fb38069e367399e93a6a073237b96
Katya Demidova <demidovakatya@gmail.com>
2016-12-20 15:55:59 +0300

    Create testi_ru.py

---
+++ testi_ru.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+questions = open('data/parsed_questions.txt').read()
+questions = questions.splitlines()
+questions_only = list(filter(lambda x: x.endswith('?'), questions))
+questions_only
+
+import pymarkovchain
+mc = pymarkovchain.MarkovChain()
+mc.generateDatabase('\n'.join(questions_only))
+mc.generateString()
